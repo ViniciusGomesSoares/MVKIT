7691bce1991a419f1933668e032246000dc3f1ff
kyuu <kyuubyn@gmail.com>
2025-04-24 15:28:52 -0300

    Carrinho de compras implementado e funcional

--- Sistema de cadastro V4.0.5/carrinho.py
+++ Sistema de cadastro V4.0.5/carrinho.py
@@ -1,19 +1,23 @@
 from flask import Blueprint, redirect, request, session, render_template, url_for # type: ignore
-from cadrestaurante import table_user
+
 
 carrinho_bp = Blueprint("carrinho", __name__, template_folder="templates")
 
 @carrinho_bp.route("/add_cart", methods=["POST"])
 def add_cart():
-    produto = request.form["teste"]
-    novo_produto = {"Produto": produto, "Valor": 10, "Quantidade": 1}
+    produto = request.form["name"]
+    valor = float(request.form["valor"])
+    novo_produto = {"Produto": produto, "Valor": valor, "Quantidade": 1}
+    
     if 'carrinho' not in session:
         session['carrinho'] = []
+
     carrinho = session['carrinho']
     carrinho.append(novo_produto)
     session['carrinho'] = carrinho
 
-    return redirect("/teste_carrinho")
+    return redirect("/carrinho")
+
 
 @carrinho_bp.route("/remove_item/<int:index>", methods=["POST"])
 def remove_item(index):
@@ -21,4 +25,4 @@ def remove_item(index):
     if 0 <= index < len(carrinho):
         carrinho.pop(index)
         session['carrinho'] = carrinho
-    return redirect("/teste_carrinho")
+    return redirect("/carrinho")
